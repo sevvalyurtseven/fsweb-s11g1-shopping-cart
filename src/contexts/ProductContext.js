79cb37551ca0402b81c63b8ae79138c420932b6c
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-08 19:21:45 +0300

    add api management to products context

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,13 +1,38 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { data } from "../data";
 import { CartContext } from "./CartContext";
+import axios from "axios";
 
 const ProductContextProvider = ({ children }) => {
   const [products, setProducts] = useState(data);
   const { addItem } = useContext(CartContext);
+  const [productAPI, setProductAPI] = useState({
+    loading: true,
+    error: false,
+  });
+
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((response) => {
+        setProducts(response.data);
+        setProductAPI({
+          loading: false,
+          error: false,
+        });
+      })
+      .catch((err) => {
+        setProductAPI({
+          loading: false,
+          error: err.message || "Something went wrong",
+        });
+      });
+  }, []);
   return (
-    <ProductContext.Provider value={{ products, addItem }}>
-      {children}
+    <ProductContext.Provider value={{ products, addItem, productAPI }}>
+      {productAPI.error && <p>{productAPI.error}</p>}
+      {productAPI.loading && <p>Loading...</p>}
+      {!productAPI.loading && !productAPI.error && children}
     </ProductContext.Provider>
   );
 };
