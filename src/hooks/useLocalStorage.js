91784d8d11b9a5422649d308bd055333e636f224
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-08 16:02:37 +0300

    create custom hook

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,16 @@
+export default function useLocalStorage(key, initialValue) {
+  const [data, setData] = useState(() => {
+    const storedValue = JSON.parse(localStorage.getItem(key));
+    if (storedValue == null) {
+      localStorage.setItem(key, JSON.stringify(initialValue));
+      return initialValue;
+    } else {
+      return storedValue;
+    }
+  });
+  const handleChange = (newValue) => {
+    localStorage.setItem(key, JSON.stringify(newValue));
+    setData(newValue);
+  };
+  return [data, handleChange];
+}
