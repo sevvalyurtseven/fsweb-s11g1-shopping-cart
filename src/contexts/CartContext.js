b6727a875b888b96ab756a6dd592bfa52d4c7016
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-08 15:09:59 +0300

    cardcontext olusturuldu

---
+++ src/contexts/CartContext.js
@@ -0,0 +1,19 @@
+import { createContext } from "react";
+
+const CartContextProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addItem = (item) => {
+    // verilen itemi sepete ekleyin
+    setCart([...cart, item]);
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addItem }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const CartContext = createContext();
+export default CartContextProvider;
