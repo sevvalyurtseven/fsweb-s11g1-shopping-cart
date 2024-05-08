4a75e2e1754b41d2f8a2353f15ac99e0e25b4342
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-08 15:35:38 +0300

    useContext ile cart stateini çeker

--- src/components/Products.js
+++ src/components/Products.js
@@ -19,7 +19,7 @@ const ScProducts = styled.section`
   }
 `;
 
-const Products = () => {
+const Products = (props) => {
   const { products, addItem } = useContext(ProductContext);
   return (
     <ScProducts>
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ScCartCheckout } from "./scParts";
 
 // Components
 import Item from "./ShoppingCartItem";
+import { CartContext } from "../contexts/CartContext";
 
 const ShoppingCart = (props) => {
+  const { cart } = useContext(CartContext);
+
   const getCartTotal = () => {
-    return props.cart
+    return cart
       .reduce((acc, value) => {
         return acc + value.price;
       }, 0)
